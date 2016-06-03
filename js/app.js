d4fe7b685a009b563a60769bfe27cbff526456ad
Omar Jbara <omar.jbara2@gmail.com>
2016-06-03 19:56:26 +0200

    Added conversation element

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var main = function () {
 
+
 	$(document).scroll(function () {
 		var scrollPos = $(window).scrollTop();
 		var header = $('header');
@@ -9,11 +10,27 @@ var main = function () {
 		} else {
 			$(header).removeClass('collapsed');
 			$('header .col-sm-8').addClass('hidden')
-
-
 		}
 
 
+	});
+
+	$('#openConv').click(function(e) {
+		var messages = $('.conversation .message, .action-message');
+		var i = 100;
+		$(messages).each(function(){
+			i += 300;
+			$(this).addClass('animated');
+			if($(this).index() == messages.length){
+				$(this).prev().addClass('animated').addClass('fadeInUp').css({"animation-delay" : i + "ms", "animation-duration": "1500ms"})
+				$(this).addClass('fadeInUp').css({"animation-delay" : i + "ms", "animation-duration": "1500ms"})
+			}
+			else if($(this).index() % 2 == 0 ){
+				$(this).addClass('fadeInLeft').css({"animation-delay" : i + "ms", "animation-duration": "700ms"});
+			} else {
+				$(this).addClass('fadeInRight').css({"animation-delay" : i + "ms", "animation-duration": "700ms"});
+			}
+		});
 	});
 
 	$('.timeline').timelify({
@@ -23,6 +40,7 @@ var main = function () {
 		animSpeed: 600,
 		offset: 150
 	});
+
 
 };
 
